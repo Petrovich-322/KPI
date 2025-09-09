f1ccf100c27051d227e4a751d64d8a5a799bb23e
Petrovich-322 <podgornijehor@gmail.com>
2025-09-09 20:39:46 +0300

    1

---
+++ Prog/Lab/1/data_types.js
@@ -0,0 +1,20 @@
+let list = [
+    true, 'hello', 5, 12, -200, false, false, 'word', true, 322, 52, '1',
+]
+let obj = {string: 0, boolean: 0, number: 0};
+for(i in list) obj[typeof(list[i])]++;
+
+console.dir({obj});
+
+list = [ 
+    "Petrovich", 42, true, "Vlad", 14, false, "+", "Dota", 322,      
+]   
+obj={};
+
+for(i in list){
+    const type = typeof(list[i]);
+    if(type in obj)obj[type]++;
+    else obj[type]=1;
+}
+
+console.dir({obj});
