7207f26f90315eecfc2f4512e58a75ab2f52ad94
Petrovich-322 <podgornijehor@gmail.com>
2025-09-09 19:35:15 +0300

    lab1

---
+++ Scalar1.js
@@ -0,0 +1,16 @@
+function inc (n) {
+    return n+=321;
+}
+const a = 1;
+const b = inc(a);
+console.dir({a,b});
+
+{
+    const obj = { n: 5 };
+    function inc (num) {
+        num.n++;
+    }
+    inc(obj);
+    console.dir(obj);
+}
+
---
+++ data_types.js
@@ -0,0 +1,46 @@
+let list = [
+    true,
+    'hello',
+    0,
+    15,
+    2,
+    false,
+    false,
+    'word',
+    -322,
+    'mops',
+    true,
+];
+
+let obj = {string: 0, boolean: 0, number: 0};
+
+for (const i in list){ 
+    const type = typeof(list[i]);
+    obj[type]++;
+}
+
+console.dir({obj});
+
+list = [
+    14,
+    'Vlad',
+    8,
+    'Petrovich',
+    'IM',
+    52,
+    true,
+    'Deutchland',
+    88,
+    true,
+];
+
+obj = {};
+
+for (const i in list){ 
+    const type = typeof(list[i]);
+    if((obj[type]==undefined))obj[type]=1;
+    else obj[type]++;
+}       
+
+console.dir({obj});
+
