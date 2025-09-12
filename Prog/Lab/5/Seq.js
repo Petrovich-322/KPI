2da67a2f9e39418328ee60f035c3b20205578ac5
Petrovich-322 <podgornijehor@gmail.com>
2025-09-12 16:33:57 +0300

    4

---
+++ Prog/Lab/5/Seq.js
@@ -0,0 +1,29 @@
+const func = [];
+//console.log(typeof(5) == "number");
+function seq(data) {
+    //console.log(data, typeof(data));
+    if(typeof(data) == "function") {
+        func.push(data);
+        return seq; 
+    } 
+    else if(typeof(data) == "number") {
+        let point = func.length - 1, res = data;
+        while(point >= 0) {
+            const localFunc = func[point];
+            //console.dir(func[point]);
+            //console.log(res);
+            res = localFunc(res);
+            point--;
+        }
+        return res;
+        //return func.reduceRight((res, localFunc) => localFunc(res), data); 
+
+    }
+}
+
+console.log(
+    seq(x => x + 7)
+    (x => x * 2)
+    (5)
+);
+
